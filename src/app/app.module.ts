9f531a038128724518dbc35ed74056ced1db245d
Chirag Shinde <shinde.chirag2204@gmail.com>
2017-10-21 21:50:28 +0530

    Configuring Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MaterialModule, MATERIAL_SANITY_CHECKS } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [{
+            provide: MATERIAL_SANITY_CHECKS,
+            useValue: false
+            }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
